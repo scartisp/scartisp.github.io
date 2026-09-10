4d99c2ef6d417f319ff6096b39667c8d0327c809
scartisp <simion.cartis1@gmail.com>
2026-09-10 12:31:32 -0400

    another lesson

--- csce242/exercises/04JavaScript/scripts/intro.js
+++ csce242/exercises/04JavaScript/scripts/intro.js
@@ -1,6 +1,19 @@
 
-
+//shows message when btn clicked
 document.getElementById('btn-show-message').onclick = (e) => {
   document.getElementById('p-message').innerHTML = 'Hello world';
   e.target.innerHTML = 'done';
+};
+
+
+//styles the link when it's clicked
+document.getElementById('link').onclick = (e) => {
+  console.log('link clicked');
+  e.preventDefault(); //don't go to links destination 
+  e.target.classList.add('cool-link');
+
+};
+
+document.getElementById('ball-btn').onclick = () => {
+  document.getElementById('ball').classList.toggle('bouncing-ball');
 };
