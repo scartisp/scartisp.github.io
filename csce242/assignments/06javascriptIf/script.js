6f1a76eb62b07a77621d6557fd5efb1035c776b7
scartisp <simion.cartis1@gmail.com>
2026-09-17 20:52:12 -0400

    renamed html elements

--- csce242/assignments/06javascriptIf/script.js
+++ csce242/assignments/06javascriptIf/script.js
@@ -1,8 +1,8 @@
 //reused elements
 const arrow = document.getElementById('arrow-i');
 const dropdownMenu = document.getElementById('dropdown-menu-ul');
-const missingClasses = document.getElementById('missing-class');
-const endSemester = document.getElementById('end-semester');
+const exerciseOne = document.getElementById('exercise-one-section');
+const exerciseTwo = document.getElementById('exercise-two-section');
 //show dropdown menu
 arrow.onclick = () => {
   arrow.classList.toggle('arrow-down');
@@ -12,17 +12,19 @@ arrow.onclick = () => {
 
 //toggle what exercise is showing
 document.getElementById('exercise-one-li').onclick = () => {
-  missingClasses.classList.remove('display-none');
-  missingClasses.classList.add('display-flex');
+  exerciseOne.classList.remove('display-none');
+  exerciseOne.classList.add('display-flex');
 
-  endSemester.classList.remove('display-flex');
-  endSemester.classList.add('display-none');
+  exerciseTwo.classList.remove('display-flex');
+  exerciseTwo.classList.add('display-none');
 };
 
 document.getElementById('exercise-two-li').onclick = () => {
-  endSemester.classList.remove('display-none');
-  endSemester.classList.add('display-flex');
+  exerciseTwo.classList.remove('display-none');
+  exerciseTwo.classList.add('display-flex');
   
-  missingClasses.classList.remove('display-flex');
-  missingClasses.classList.add('display-none');
+  exerciseOne.classList.remove('display-flex');
+  exerciseOne.classList.add('display-none');
 };
+
+//exercise one
