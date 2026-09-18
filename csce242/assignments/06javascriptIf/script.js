4d9c4244fe6122ad5ff0fd4b2386ca968a6b465a
scartisp <simion.cartis1@gmail.com>
2026-09-17 21:42:33 -0400

    pretty much completed js if assignment

--- csce242/assignments/06javascriptIf/script.js
+++ csce242/assignments/06javascriptIf/script.js
@@ -1,9 +1,6 @@
-//reused elements
+//show dropdown menu
 const arrow = document.getElementById('arrow-i');
 const dropdownMenu = document.getElementById('dropdown-menu-ul');
-const exerciseOne = document.getElementById('exercise-one-section');
-const exerciseTwo = document.getElementById('exercise-two-section');
-//show dropdown menu
 arrow.onclick = () => {
   arrow.classList.toggle('arrow-down');
   arrow.classList.toggle('arrow-up')
@@ -11,6 +8,8 @@ arrow.onclick = () => {
 }
 
 //toggle what exercise is showing
+const exerciseOne = document.getElementById('exercise-one-section');
+const exerciseTwo = document.getElementById('exercise-two-section');
 document.getElementById('exercise-one-li').onclick = () => {
   exerciseOne.classList.remove('display-none');
   exerciseOne.classList.add('display-flex');
@@ -22,9 +21,54 @@ document.getElementById('exercise-one-li').onclick = () => {
 document.getElementById('exercise-two-li').onclick = () => {
   exerciseTwo.classList.remove('display-none');
   exerciseTwo.classList.add('display-flex');
-  
+
   exerciseOne.classList.remove('display-flex');
   exerciseOne.classList.add('display-none');
 };
 
 //exercise one
+classesMissedComment = document.getElementById('classes-missed-comment-p');
+
+let classesMissed;
+const totalClasses = 25;
+const attendencePercent = 0.07;
+let percentMissingAttendance;
+document.getElementById('exercise-one-input').addEventListener('input', (event) => {
+  classesMissed = event.target.value;
+  percentMissingAttendance = (classesMissed / totalClasses * attendencePercent * 100).toFixed(1);
+
+  document.getElementById('percent-lost-p').classList.remove('display-none');
+  document.getElementById('percent-lost-span').innerHTML = percentMissingAttendance;
+  document.getElementById('classes-skipped-span').innerHTML = classesMissed;
+
+  if (classesMissed === 0)
+    classesMissedComment.innerHTML = "you're attending all classes! Great job!";
+  else if (classesMissed >= 1 && classesMissed <= 3)
+    classesMissedComment.innerHTML = `you're missing ${classesMissed} classes. Ok, but don't turn it into a habit.`;
+  else if (classesMissed >= 4 && classesMissed <= 8)
+    classesMissedComment.innerHTML = `${classesMissed} classes? This isn't an online class. do better.`;
+  else if (classesMissed > 8)
+    classesMissedComment.innerHTML = `${classesMissed} classes? Just drop out.`;
+});
+
+//exercise two
+const MSPERDAY = 1000 * 60 * 60 * 24;
+
+const daysRemainingComment = document.getElementById('days-remaining-comment-p');
+
+const endDay = new Date(2026, 11, 4);
+let currDay = new Date();
+let daysRemainingMs = endDay - currDay;
+const daysRemaining = Math.round(daysRemainingMs / MSPERDAY);
+
+document.getElementById('days-left-span').innerHTML = daysRemaining;
+
+if (daysRemaining <= 0) {
+  daysRemainingComment.innerHTML = "it's over, you did it!";
+}
+else if (daysRemaining > 0 && daysRemaining <= 10)
+  daysRemainingComment.innerHTML = 'Almost done!';
+else if (daysRemaining > 10 && daysRemaining <= 30) {
+  daysRemainingComment.innerHTML = "You've still got a ways to go.";
+} else
+  daysRemainingComment.innerHTML = "Not time to start counting down yet.";
